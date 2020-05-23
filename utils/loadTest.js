d4b6bec25359fda236a5b151cd13eda25e5b7ef8
chylomicronman <bmccann36@gmail.com>
2020-05-23 13:28:05 -0400

    bump up timeout for createMsgMapping make table names simpler

--- utils/loadTest.js
+++ utils/loadTest.js
@@ -7,19 +7,19 @@ for (let i = 0; i < 10000; i++) {
   userIds.push(uuid());
 }
 
-// const GATEWAY_URL =
-//   "https://828jc96lqg.execute-api.us-east-1.amazonaws.com/dev/messageContent";
+const GATEWAY_URL =
+  "https://e7i538cxc5.execute-api.us-east-1.amazonaws.com/dev/mapping";
 
-const GATEWAY_URL = "http://localhost:3000/dev/mapping";
+// const GATEWAY_URL = "http://localhost:3000/dev/mapping";
 
-// to send just 10 for smoke testing
+// ? to send just 10 for smoke testing
 // callEp(userIds.slice(0, 10))
 //   .then((res) => {
 //     console.log(res.data);
 //   })
 //   .catch(err => console.log(err));
 
-// send 10k
+// ! send 10k
 callEp(userIds)
   .then((res) => {
     console.log(res.data);
